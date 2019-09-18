6ad27d53c9ae5868275c3e2f1fb178af38962548
Akash-Anand10 <akash10.anand@gmail.com>
2019-09-18 17:20:02 +0530

    seperated the runner file from app.

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ def hello():
     return "hello world"
 
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+# if __name__ == '__main__':
+#     app.run(debug=True, use_reloader=True)
